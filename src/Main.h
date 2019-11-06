7cc02434004b4deddfd26e8bb2f4f9ef16a13fc4
Matt Filetto <matt.filetto@gmail.com>
2019-11-06 04:01:45 -0800

    const
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/Main.h
+++ src/Main.h
@@ -60,7 +60,7 @@ d4rk@xbmc.org
 
 #include <libprojectM/projectM.hpp>
 
-extern projectM* m_projectM;
+
 
 class ATTRIBUTE_HIDDEN CVisualizationProjectM
   : public kodi::addon::CAddonBase,
@@ -70,6 +70,7 @@ public:
   CVisualizationProjectM();
   ~CVisualizationProjectM() override;
 
+  const projectM* m_projectM;
   void Render() override;
   void AudioData(const float* audioData, int audioDataLength, float *freqData, int freqDataLength) override;
   bool GetPresets(std::vector<std::string>& presets) override;
