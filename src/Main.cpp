7ed119de0b4a6728bb7f903c019ba9753f6d3102
Matt Filetto <matt.filetto@gmail.com>
2019-11-05 18:31:20 -0800

    test
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/Main.cpp
+++ src/Main.cpp
@@ -55,6 +55,9 @@ d4rk@xbmc.org
 
 #include "Main.h"
 
+//m_projectM;
+projectM* m_projectM;
+
 //-- Create -------------------------------------------------------------------
 // Called once when the visualisation is created by XBMC. Do any setup here.
 //-----------------------------------------------------------------------------
